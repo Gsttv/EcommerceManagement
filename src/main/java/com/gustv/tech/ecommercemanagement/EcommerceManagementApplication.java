54d824dbb9770c3f38fa5627ed5e5d7ac85d24f4
Gustavo Morais <gustvmorais@gmail.com>
2024-09-02 17:56:33 -0300

    Firt Config

---
+++ src/main/java/com/gustv/tech/ecommercemanagement/EcommerceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gustv.tech.ecommercemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustv/tech/ecommercemanagement/EcommerceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustv.tech.ecommercemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
